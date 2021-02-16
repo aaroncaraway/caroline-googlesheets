1c07d6a35d8e27c9df2316734fe8628822837126
aaroncaraway <aaroncaraway42@gmail.com>
2021-02-15 18:15:01 -0800

    Add embedded youtube videos

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,7 @@
 // eslint-disable jsx-props-no-spreading
 import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import parse from 'html-react-parser';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { fade, makeStyles } from '@material-ui/core/styles';
@@ -12,15 +13,22 @@ function Dashboard() {
   const classes = useStyles();
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState();
+  const [videos, setVideos] = useState();
+  const [sheet, setSheet] = useState(2);
 
   useEffect(() => {
     const fetchData = async () => {
-      const url =
-        'https://spreadsheets.google.com/feeds/cells/1qmBlKnDlVFDJH3r3EOEB4bZiga6nIvIYOE1-5_MxNI4/1/public/values?alt=json';
+      const url = `https://spreadsheets.google.com/feeds/cells/1qmBlKnDlVFDJH3r3EOEB4bZiga6nIvIYOE1-5_MxNI4/${sheet}/public/values?alt=json`;
       const result = await axios(url);
       console.log(result.data);
       console.log(result.data.feed.entry);
+
       setData(result.data.feed.entry);
+      setVideos(
+        result.data.feed.entry.filter((item) =>
+          item.content['$t'].includes('iframe') ? item.content['$t'] : ''
+        )
+      );
       setLoading(false);
     };
     fetchData();
@@ -36,6 +44,8 @@ function Dashboard() {
       ) : (
         <>
           {data && data.map((item) => <h3>{item.content['$t']}</h3>)}
+          {videos &&
+            videos.map((video) => parse(`<div>${video.content['$t']}</div>`))}
           {/* <Gallery photos={data.photos} /> */}
         </>
       )}
